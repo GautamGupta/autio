be26ccbb7b3ce4e0e74a0aa441ec2ab70fe19587
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-01-17 11:19:48 -0500

    new stuff

--- app/src/main/java/me/autio/autio/SpotifyActivity.java
+++ app/src/main/java/me/autio/autio/SpotifyActivity.java
@@ -3,6 +3,11 @@ package me.autio.autio;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.app.Activity;
 
 /**
  * Created by Ivan on 2015-01-17.
