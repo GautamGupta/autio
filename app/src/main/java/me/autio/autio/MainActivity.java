7088ed03ad29abe25f2ce146ffee7658c2aebf2c
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-01-17 23:36:00 -0500

    google for spotify

--- app/src/main/java/me/autio/autio/MainActivity.java
+++ app/src/main/java/me/autio/autio/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("");
         setContentView(R.layout.activity_main);
     }
 
--- app/src/main/java/me/autio/autio/SearchActivity.java
+++ app/src/main/java/me/autio/autio/SearchActivity.java
@@ -1,51 +1,51 @@
 package me.autio.autio;
 
-import android.app.Activity;
-import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.*;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.SearchView;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 /**
  * Created by Ivan on 2015-01-17.
  */
-public class SearchActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+public class SearchActivity extends ActionBarActivity{
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            // doMySearch(query);
+    }
+    EditText et = (EditText) findViewById(R.id.search);
+    StringBuilder query = new StringBuilder();
+    et.setOnEditorActionListener (new TextView.OnEditorActionListener(){
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            boolean handled = false;
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                searchSpotify(query.toString());
+                handled = true;
+                query = new StringBuilder();
+            }
+            else
+            {
+                if(query.length() > 0){
+                    searchSpotify(query.toString());
+                }
+                query.append(event.getCharacters().substring(event.getCharacters().lenth() - 1));
+                handled = false;
+            }
+            return handled;
         }
-    }
+    });
 
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search_bar).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-
-        return true;
-
-    }
 }
+
+
